8380a6a5f3a975668b1fac68b9c1a4d06d84f001
Thomas Chappell UVU Laptop F2024 <jthomaschappell@gmail.com>
2024-11-21 12:49:30 -0700

    changed top side bar to have better padding

--- lib/screens/loan_dashboard_screen.dart
+++ lib/screens/loan_dashboard_screen.dart
@@ -15,15 +15,26 @@ class _LoanDashboardScreenState extends State<LoanDashboardScreen> {
         color: Colors.grey[400],
         borderRadius: BorderRadiusDirectional.circular(12),
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          for (var item in ['Home', 'Notifications', 'User Config', 'Settings'])
-            TextButton(
-              onPressed: () {},
-              child: Text(item, style: const TextStyle(color: Colors.white)),
-            ),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 60.0,
+          right: 60.0,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            for (var item in [
+              'Home',
+              'Notifications',
+              'User Config',
+              'Settings'
+            ])
+              TextButton(
+                onPressed: () {},
+                child: Text(item, style: const TextStyle(color: Colors.white)),
+              ),
+          ],
+        ),
       ),
     );
   }
@@ -35,7 +46,10 @@ class _LoanDashboardScreenState extends State<LoanDashboardScreen> {
         children: [
           Padding(
             padding: const EdgeInsets.only(
+              top: 16.0,
               bottom: 16.0,
+              left: 8.0,
+              right: 8.0,
             ),
             child: _buildTopNav(),
           ),
@@ -43,7 +57,9 @@ class _LoanDashboardScreenState extends State<LoanDashboardScreen> {
             child: Row(
               children: [
                 Container(
-                    width: 250, color: Colors.grey[200]), // Placeholder sidebar
+                  width: 250,
+                  color: Colors.grey[200],
+                ), // Placeholder sidebar
                 const Expanded(child: Center(child: Text('Main Content'))),
               ],
             ),

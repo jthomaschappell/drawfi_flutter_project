ede2de88e0f0e7244264873f6e74a4a8dec602f0
Thomas Chappell UVU Laptop F2024 <jthomaschappell@gmail.com>
2024-11-21 12:41:01 -0700

    fixing layout

--- lib/screens/loan_dashboard_screen.dart
+++ lib/screens/loan_dashboard_screen.dart
@@ -8,8 +8,48 @@ class LoanDashboardScreen extends StatefulWidget {
 }
 
 class _LoanDashboardScreenState extends State<LoanDashboardScreen> {
+  Widget _buildTopNav() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      decoration: BoxDecoration(
+        color: Colors.grey[400],
+        borderRadius: BorderRadiusDirectional.circular(12),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          for (var item in ['Home', 'Notifications', 'User Config', 'Settings'])
+            TextButton(
+              onPressed: () {},
+              child: Text(item, style: const TextStyle(color: Colors.white)),
+            ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              bottom: 16.0,
+            ),
+            child: _buildTopNav(),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Container(
+                    width: 250, color: Colors.grey[200]), // Placeholder sidebar
+                const Expanded(child: Center(child: Text('Main Content'))),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
